9f1630fc47a47877924f8f8f41f57a7382fcf80d
Ryan Codrai <ryan.codrai@gmail.com>
2015-10-20 12:09:00 +0100

    Added checklist and added multiple command support. Also added attack support.

---
+++ game3/entities.py
@@ -0,0 +1,10 @@
+entity_zombie = {
+    "id": "zombie",
+    "alive": True,
+    "name": "a zombie",
+    "health": 75,
+    "attack": 25
+    "description": "The zombie looks familiar."
+}
+
+entities = [entity_zombie]
--- game3/player.py
+++ game3/player.py
@@ -1,11 +1,13 @@
 from items import *
 from map import rooms
-
-inventory = [item_id, item_laptop, item_money]
-moves = 0
+player = {
+"inventory":[item_id, item_laptop, item_money],
+"equpied_weapon": item_laptop,
+"health" :100,
+"moves" :0,
 # Start game at the reception
-current_room = rooms["Reception"]
+"current_room": rooms["Reception"]
 # Work out the inital weight of item in the players invientory.
-inventory_mass = 0
+"inventory_mass": 0}
 for item in inventory:
     inventory_mass += item["mass"]
