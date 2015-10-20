66045ce11f65132f01870bf5fbbacddd945612d7
dannstuff <daniel96morgan@hotmail.com>
2015-10-20 15:16:26 +0100

    boolean value added to items

--- game3/items.py
+++ game3/items.py
@@ -2,78 +2,90 @@ item_red_flare = {
     "id": "red flare",
     "name": "a red flare",
     "description": "You managed to find the red flare. Wow aren't you a \
-    special snowflake!"
+    special snowflake!", 
+    "carriable" : True
 }
 
 item_blue_flare = {
     "id": "blue flare",
     "name": "a blue flare",
-    "description": "You even found the blue flare! Keep up the good work!"
+    "description": "You even found the blue flare! Keep up the good work!", 
+    "carriable" : True
 }
 
 item_pistol = {
     "id": "pistol",
     "name": "a pistol",
     "description": "You found a brand new kind of dirty pistol. Be careful! \
-     Don't kill too many people."
+     Don't kill too many people.", 
+    "carriable" : True
 }
 
 item_dress = {
     "id": "dress",
     "name": "a little cocktail dress",
     "description": "Wow you found a little pink cocktail dress! It really \
-     brings out the colour in your eyes!"
+     brings out the colour in your eyes!", 
+    "carriable" : False
 }
 
 item_shoes = {
     "id": "shoes",
     "name": "a pair of old shoes",
-    "description": "This pair of shoes goes amazingly with the dress!"
+    "description": "This pair of shoes goes amazingly with the dress!", 
+    "carriable" : False
 }
 
 item_keys = {
     "id": "keys",
     "name": "a set of keys",
-    "description": "I wonder if they unlock anything."
+    "description": "I wonder if they unlock anything.", 
+    "carriable" : True
 }
 
 item_torch = {
     "id": "torch",
     "name": "a torch",
-    "description": "Oh look this torch is brighter than my future."
+    "description": "Oh look this torch is brighter than my future.", 
+    "carriable" : True
 }
 
 item_batteries = {
     "id": "batteries",
     "name": "some batteries",
-    "description": "Why would you need batteries ?? Hmm, we'll see."
+    "description": "Why would you need batteries ?? Hmm, we'll see.", 
+    "carriable" : True
 }
 item_water_gun = {
      "id": "water gun",
      "name": "a water gun",
      "description": "not the most useful item in the current situation, maybe \
-     you could use it in the event of a very..very small fire?"
+     you could use it in the event of a very..very small fire?", 
+    "carriable" : False
 }
 
 item_medipac = {
     "id": "medipac",
     "name": "a medipac",
     "description": "This medipac could come in handy in the event of a zombie \
-    attack! You should keep it safe."
+    attack! You should keep it safe.", 
+    "carriable" : True
 }
 
 item_billy_idol_cd = {
     "id": "cd",
     "name": "a Billy Idol CD",
     "description": "Your favourite CD, a must have in the event of a zombie \
-    apocalypse"
+    apocalypse", 
+    "carriable" : True
 }
 
 item_saucepan = {
     "id": "saucepan",
     "name": "a Saucepan",
     "description": "this could be used to cook food...or kill zombies, \
-    you chose."
+    you chose.", 
+    "carriable" : False
 }
 
 items = {
